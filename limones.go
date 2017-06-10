9a7d3fcb1ca41af4e32d8739a0bfc2ae4dba096a
Daniel Stamer <daniel@stamer.info>
2017-06-10 10:00:47 +0000

    lefthand side of bar
    Signed-off-by: Daniel Stamer <daniel@stamer.info>

---
+++ limones.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+type Item struct {
+	Cache  string
+	Update Updater
+}
+
+type Updater func(i *Item)
+
+func (i *Item) Start() {
+	go i.Update(i)
+}
+
+func Compose(items map[string]*Item) (out string) {
+	const sep string = " | "
+	return "%{l} " + items["host"].Cache + sep + items["desktop"].Cache + sep + items["cpu"].Cache + sep + items["memory"].Cache + sep + items["battery"].Cache
+}
+
+func Command(name string, args ...string) string {
+	out, _ := exec.Command(name, args...).Output()
+	return strings.TrimSpace(string(out))
+}
+
+func main() {
+
+	items := make(map[string]*Item)
+
+	items["host"] = &Item{"", func(i *Item) {
+		for {
+			i.Cache, _ = os.Hostname()
+			time.Sleep(time.Second * time.Duration(100))
+		}
+	}}
+	items["host"].Start()
+	items["desktop"] = &Item{"", func(i *Item) {
+		for {
+			i.Cache = Command("bash", "-c", "xprop -root _NET_CURRENT_DESKTOP | awk '{print $3+1}'")
+			time.Sleep(time.Second * time.Duration(2))
+		}
+	}}
+	items["desktop"].Start()
+	items["cpu"] = &Item{"", func(i *Item) {
+		for {
+			var buffer bytes.Buffer
+			buffer.WriteString("Cpu: ")
+			buffer.WriteString(strings.TrimSpace(Command("bash", "-c", "echo $[100-$(vmstat 1 2|tail -1|awk '{print $15}')]")))
+			buffer.WriteString("% ")
+			buffer.WriteString(strings.TrimSpace(Command("bash", "-c", "sensors | grep thinkpad-isa-0000 -A 5 | grep temp1 | grep -o '+[0-9]*\\.[0-9]'")))
+			buffer.WriteString("C ")
+			buffer.WriteString(strings.TrimSpace(Command("bash", "-c", "sensors | grep thinkpad-isa-0000 -A 5 | grep fan1 | grep -o '[0-9]* RPM'")))
+			i.Cache = buffer.String()
+			time.Sleep(time.Second * time.Duration(2))
+		}
+	}}
+	items["cpu"].Start()
+	items["memory"] = &Item{"", func(i *Item) {
+		for {
+			var buffer bytes.Buffer
+			buffer.WriteString("Mem: ")
+			buffer.WriteString(Command("bash", "-c", "free -m | awk 'NR==2{printf \"%.f%%\", $3*100/$2 }'"))
+			i.Cache = buffer.String()
+			time.Sleep(time.Second * time.Duration(2))
+
+		}
+	}}
+	items["memory"].Start()
+	items["battery"] = &Item{"", func(i *Item) {
+		for {
+			var buffer bytes.Buffer
+			buffer.WriteString("Bat: ")
+			buffer.WriteString(Command("cat", "/sys/class/power_supply/BAT0/capacity"))
+			buffer.WriteString("%")
+			i.Cache = buffer.String()
+			time.Sleep(time.Second * time.Duration(20))
+
+		}
+	}}
+	items["battery"].Start()
+	items["kernel"] = &Item{"", func(i *Item) {
+		for {
+			i.Cache = Command("uname", "-r")
+			time.Sleep(time.Second * time.Duration(100))
+
+		}
+	}}
+	items["kernel"].Start()
+
+	time.Sleep(time.Second * time.Duration(2))
+	for {
+		fmt.Println(Compose(items))
+		// Sleep a second
+		time.Sleep(time.Second * time.Duration(1))
+	}
+}
